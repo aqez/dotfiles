c60b5d11ed46af7ad1f06b1a9ba6340566e6c858
aqez <tony@undauntedonline.com>
2022-07-14 11:47:00 -0400

    Awesome remove main button, add formatting in cs files on save

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -96,27 +96,6 @@ awful.layout.layouts = {
 -- }}}
 
 -- {{{ Menu
--- Create a launcher widget and a main menu
-local myawesomemenu = {
-    { "hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
-    { "manual", terminal .. " -e man awesome" },
-    { "edit config", editor_cmd .. " " .. awesome.conffile },
-    { "restart", awesome.restart },
-    { "quit", function() awesome.quit() end },
-}
-
-local mymainmenu = awful.menu({
-    items = {
-        { "awesome", myawesomemenu, beautiful.awesome_icon },
-        { "open terminal", terminal }
-    }
-})
-
-local mylauncher = awful.widget.launcher({
-    image = beautiful.awesome_icon,
-    menu = mymainmenu
-})
-
 -- Menubar configuration
 menubar.utils.terminal = terminal -- Set the terminal for applications that require it
 -- }}}
@@ -226,7 +205,6 @@ awful.screen.connect_for_each_screen(function(s)
         layout = wibox.layout.align.horizontal,
         { -- Left widgets
             layout = wibox.layout.fixed.horizontal,
-            mylauncher,
             s.mytaglist,
             s.mypromptbox,
         },
@@ -258,7 +236,6 @@ end)
 
 -- {{{ Mouse bindings
 root.buttons(gears.table.join(
-    awful.button({}, 3, function() mymainmenu:toggle() end),
     awful.button({}, 4, awful.tag.viewnext),
     awful.button({}, 5, awful.tag.viewprev)
 ))
@@ -287,8 +264,6 @@ local globalkeys = gears.table.join(
         end,
         { description = "focus previous by index", group = "client" }
     ),
-    awful.key({ modkey, }, "w", function() mymainmenu:show() end,
-        { description = "show main menu", group = "awesome" }),
 
     -- Layout manipulation
     awful.key({ modkey, "Shift" }, "j", function() awful.client.swap.byidx(1) end,
--- nvim/ftplugin/cs.vim
+++ nvim/ftplugin/cs.vim
@@ -4,6 +4,7 @@ set errorformat=\ %#%f(%l\\\,%c):\ %m
 augroup csharp
     autocmd!
 
+    autocmd BufWritePre *.cs :silent lua vim.lsp.buf.formatting()
     autocmd BufWritePre *.cs :silent! g/^\_$\n\_^$/d
     autocmd BufWritePre *.cs :silent! %s/^\(\s*{\)\n\_$/\1
     autocmd BufWritePre *.cs :silent! %s/^\_$\n\(\s*}\)/\1
