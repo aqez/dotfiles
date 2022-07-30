a5bc3b82c7654ac1142bea5bf68684149daceb88
Tony Dwire <tony@undauntedonline.com>
2022-07-30 19:58:37 -0400

    Update make hotkey to work

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -52,4 +52,4 @@ vim.keymap.set("n", "<leader>rt", ":TestNearest<CR>", opts)
 vim.keymap.set("n", "<leader>df", vim.diagnostic.open_float, opts)
 
 
-vim.keymap.set("n", "<C-b>", ":make")
+vim.keymap.set("n", "<C-b>", ":make<CR>")
