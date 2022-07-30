66173673a460c4448680861aa4c52af44084bff2
Tony Dwire <tony@undauntedonline.com>
2022-07-30 19:12:24 -0400

    Finally got preselect turned off!

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -80,28 +80,19 @@ telescope.load_extension('fzy_native')
 telescope.load_extension('ui-select')
 
 -- nvim-cmp
-local has_words_before = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 local cmp = require('cmp')
 cmp.setup({
-    confirmation = { completeopt = 'menu,menuone,noselect' },
     snippet = {
         expand = function(args)
             vim.fn["vsnip#anonymous"](args.body)
         end,
     },
+    preselect = cmp.PreselectMode.None,
     mapping = {
         ['<C-j>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
         ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
-            elseif vim.fn["vsnip#available"](1) == 1 then
-                feedkey("<plug>(vsnip-expand-or-jump)", "")
-            elseif has_words_before() then
-                cmp.complete()
             else
                 fallback()
             end
@@ -109,8 +100,6 @@ cmp.setup({
         ['<S-Tab>'] = cmp.mapping(function()
             if cmp.visible() then
                 cmp.select_prev_item()
-            elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-                feedkey("<Plug>(vsnip-jump-prev)", "")
             end
         end)
     },
