b8a067ccf36dfcd08256d98d2573827e82d3b0e4
aqez <tony@undauntedonline.com>
2022-08-05 08:29:12 -0400

    Set neo-smart-open to true

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -3,7 +3,6 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
-
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
 (setq user-full-name "Tony Dwire"
@@ -77,6 +76,7 @@
 
 (after! lsp-rust (setq lsp-rust-server 'rust-analyzer))
 (after! projectile (setq projectile-project-search-path '("~/repos")))
+(after! neotree (setq neo-smart-open t))
 
 (setq dap-auto-configure-mode t)
 (require 'dap-cpptools)
