4373e1163d48c14f1260ead623725c6014f1537a
aqez <tony@undauntedonline.com>
2022-09-08 09:18:23 -0400

    Fix brightness key

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -245,7 +245,7 @@ root.buttons(gears.table.join(
 -- {{{ Key bindings
 local globalkeys = gears.table.join(
     awful.key({}, "XF86MonBrightnessDown", function() awful.spawn.with_shell("brightnessctl s 10%-") end, { description = "Decrease brightness", group = "custom"}),
-    awful.key({}, "XF86MonBrightnessDown", function() awful.spawn.with_shell("brightnessctl s 10%+") end, { description = "Increase brightness", group = "custom"}),
+    awful.key({}, "XF86MonBrightnessUp", function() awful.spawn.with_shell("brightnessctl s 10%+") end, { description = "Increase brightness", group = "custom"}),
     awful.key({ modkey, }, "s", hotkeys_popup.show_help,
         { description = "show help", group = "awesome" }),
     awful.key({ modkey, }, "Left", awful.tag.viewprev,
