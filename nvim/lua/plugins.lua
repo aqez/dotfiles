7bf7f2a6858e66998c3b9763ecb5acbefb307eb8
Tony Dwire <tony@undauntedonline.com>
2022-11-30 22:25:02 -0500

    Update some stuff

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -107,8 +107,8 @@ cmp.setup({
     }
 })
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+--local capabilities = vim.lsp.protocol.make_client_capabilities()
+require('cmp_nvim_lsp').default_capabilities()
 
 -- LSP
 local lsp_installer = require("nvim-lsp-installer")
