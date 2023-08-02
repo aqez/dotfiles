70df10bbbfbfaa936133e97b79db36cbdd3b0f44
Tony <tony.dwire@paradigmh2o.com>
2023-08-02 16:28:58 -0400

    Center nvim-tree

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -43,7 +43,50 @@ require("packer").startup(function(use)
         requires = { "nvim-tree/nvim-web-devicons" },
         config = function()
             local nvim_tree = require("nvim-tree")
-            nvim_tree.setup()
+            --nvim_tree.setup()
+            local HEIGHT_RATIO = 0.8 -- You can change this
+            local WIDTH_RATIO = 0.5  -- You can change this too
+
+            nvim_tree.setup({
+                disable_netrw = true,
+                hijack_netrw = true,
+                respect_buf_cwd = true,
+                sync_root_with_cwd = true,
+                view = {
+                    relativenumber = true,
+                    float = {
+                        enable = true,
+                        open_win_config = function()
+                            local screen_w = vim.opt.columns:get()
+                            local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+                            local window_w = screen_w * WIDTH_RATIO
+                            local window_h = screen_h * HEIGHT_RATIO
+                            local window_w_int = math.floor(window_w)
+                            local window_h_int = math.floor(window_h)
+                            local center_x = (screen_w - window_w) / 2
+                            local center_y = ((vim.opt.lines:get() - window_h) / 2)
+                                - vim.opt.cmdheight:get()
+                            return {
+                                border = "rounded",
+                                relative = "editor",
+                                row = center_y,
+                                col = center_x,
+                                width = window_w_int,
+                                height = window_h_int,
+                            }
+                        end,
+                    },
+                    width = function()
+                        return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+                    end,
+                },
+                -- filters = {
+                --   custom = { "^.git$" },
+                -- },
+                -- renderer = {
+                --   indent_width = 1,
+                -- },
+            })
         end
     }
 
@@ -230,7 +273,7 @@ require("packer").startup(function(use)
     use {
         "shaunsingh/nord.nvim",
         config = function()
-            vim.g.nord_disable_background = true
+            -- vim.g.nord_disable_background = true
             vim.g.nord_borders = true
             require('nord').set();
         end
