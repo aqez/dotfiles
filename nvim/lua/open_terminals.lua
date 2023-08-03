cdd952348f67b702a744988df8b66cfa5ced3067
Tony <tony.dwire@paradigmh2o.com>
2023-08-03 11:31:51 -0400

    Set terminal names to a shorter name

--- nvim/lua/open_terminals.lua
+++ nvim/lua/open_terminals.lua
@@ -1,14 +1,28 @@
+local function split(inputstr, sep)
+    print("Splitting " .. inputstr .. " on " .. sep)
+    if sep == nil then
+        sep = "%s"
+    end
+    local t = {}
+    for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
+        table.insert(t, str)
+    end
+    return t
+end
+
 local function open_terminals_in_project()
     local csproj_folders = vim.fn.systemlist("find . -name 'Program.cs' -maxdepth 2 -exec dirname {} \\; | uniq")
     local node_folders = vim.fn.systemlist("find . -name 'package.json' -maxdepth 2 -exec dirname {} \\; | uniq")
 
     for k, v in pairs(csproj_folders) do
-        local cmd = string.format('tmux new-window -dS -c %s -n %s', v, v, v)
+        local names = split(v, "\\.")
+        local cmd = string.format('tmux new-window -dS -c %s -n %s', v, v, names[#names])
         vim.fn.system(cmd)
     end
 
     for k, v in pairs(node_folders) do
-        local cmd = string.format('tmux new-window -dS -c %s -n %s "npm start; fish"', v, v, v)
+        local names = split(v, "\\.")
+        local cmd = string.format('tmux new-window -dS -c %s -n %s "npm start; fish"', v, v, names[#names])
         vim.fn.system(cmd)
     end
 end
