a2ef438c75c17ff42199dbe69535df4fd25ea309
tony-dwire-paradigm <tony.dwire@paradigmh2o.com>
2024-03-10 15:25:22 -0400

    Add wezterm color switcher

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,8 +2,27 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.color_scheme = "GitHub Dark"
+local light = "Github"
+local dark = "GitHub Dark"
 
+local function toggle_light_dark(window, _)
+	local currentSchemeName = window:effective_config().color_scheme
+	local allSchemes = wezterm.color.get_builtin_schemes()
+	local currentMode = wezterm.gui.get_appearance()
+
+    local overrides = window:get_config_overrides() or {}
+
+    if overrides.color_scheme == dark then
+        overrides.color_scheme = light
+    else
+        overrides.color_scheme = dark
+    end
+
+    wezterm.log_info("Switched from " .. currentSchemeName .. " to " .. overrides.color_scheme)
+    window:set_config_overrides(overrides)
+end
+
+config.color_scheme = dark
 config.font = wezterm.font("0xProto Nerd Font Mono")
 config.font_size = 15.0
 config.enable_tab_bar = false
@@ -19,7 +38,13 @@ config.keys = {
         key = "r",
         mods = "CMD|SHIFT",
         action = wezterm.action.ReloadConfiguration
-    }
+    },
+
+    -- Theme Cycler
+    { key = "t", mods = "ALT", action = wezterm.action_callback(toggle_light_dark) },
+
+    -- Look up Scheme you switched to
+    { key = "Escape", mods = "CTRL", action = wezterm.action.ShowDebugOverlay },
 }
 
 return config
