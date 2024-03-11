bed6b72839df2a02187cf721c57dd98fa99c81b8
Tony Dwire <tony@undauntedonline.com>
2024-03-11 18:57:10 -0400

    Add nvim-dap-go and clean up installed package list

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -373,6 +373,12 @@ require("lazy").setup({
             }
         end
     },
+    { "leoluz/nvim-dap-go",
+        dependencies = { "mfussenegger/nvim-dap" },
+        config = function()
+            require("dap-go").setup()
+        end
+    },
     {
         "rcarriga/nvim-dap-ui",
         dependencies = { "mfussenegger/nvim-dap" },
