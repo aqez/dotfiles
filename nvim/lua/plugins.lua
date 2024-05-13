83fc35815f70b338b98faea9fe5c3a64764e9ac5
Tony Dwire <tony@undauntedonline.com>
2024-05-13 19:59:16 -0400

    Add parpar back

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -21,6 +21,25 @@ require("lazy").setup({
     "pbrisbin/vim-colors-off",
     "vim-test/vim-test",
     "github/copilot.vim",
+    {
+        "dundalek/parpar.nvim",
+        dependencies = { "gpanders/nvim-parinfer", "julienvincent/nvim-paredit" },
+        config = function()
+            local paredit = require("nvim-paredit")
+            require("parpar").setup {
+                paredit = {
+                    -- pass any nvim-paredit options here
+                    keys = {
+                        -- custom bindings are automatically wrapped
+                        ["<A-H>"] = { paredit.api.slurp_backwards, "Slurp backwards" },
+                        ["<A-J>"] = { paredit.api.barf_backwards, "Barf backwards" },
+                        ["<A-K>"] = { paredit.api.barf_forwards, "Barf forwards" },
+                        ["<A-L>"] = { paredit.api.slurp_forwards, "Slurp forwards" },
+                    }
+                }
+            }
+        end
+    },
     { "rcarriga/nvim-notify",
         config = function()
             vim.notify = require("notify")
