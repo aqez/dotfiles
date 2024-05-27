228f26b93011ff7bd884025b7dc3d7145874dde3
Tony Dwire <tony@undauntedonline.com>
2024-05-26 22:18:43 -0400

    Re-add lisp stuff. Fix VSCode theme

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -21,6 +21,26 @@ require("lazy").setup({
     "pbrisbin/vim-colors-off",
     "vim-test/vim-test",
     "github/copilot.vim",
+    "Olical/conjure", 
+    {
+        "dundalek/parpar.nvim",
+        dependencies = { "gpanders/nvim-parinfer", "julienvincent/nvim-paredit" },
+        config = function()
+            local paredit = require("nvim-paredit")
+            require("parpar").setup {
+                paredit = {
+                    -- pass any nvim-paredit options here
+                    keys = {
+                        -- custom bindings are automatically wrapped
+                        ["<A-H>"] = { paredit.api.slurp_backwards, "Slurp backwards" },
+                        ["<A-J>"] = { paredit.api.barf_backwards, "Barf backwards" },
+                        ["<A-K>"] = { paredit.api.barf_forwards, "Barf forwards" },
+                        ["<A-L>"] = { paredit.api.slurp_forwards, "Slurp forwards" },
+                    }
+                }
+            }
+        end
+    },
     {
         "Mofiqul/vscode.nvim",
         config = function()
@@ -294,6 +314,7 @@ require("lazy").setup({
             "hrsh7th/cmp-buffer",
             "hrsh7th/cmp-nvim-lsp",
             "hrsh7th/cmp-path",
+            "PaterJason/cmp-conjure",
         },
         config = function()
             local cmp = require('cmp')
@@ -332,7 +353,7 @@ require("lazy").setup({
         "catppuccin/nvim",
         name = "catppuccin",
         config = function()
-            vim.cmd [[colorscheme catppuccin-frappe]]
+            --vim.cmd [[colorscheme catppuccin-frappe]]
         end
     },
     {
