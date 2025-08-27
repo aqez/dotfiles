dd6792f49fd07e55d8d3c1e0c503d23bc3453fe9
tony-dwire-paradigm <tony.dwire@paradigmh2o.com>
2025-08-27 11:03:21 -0400

    Remove doom stuff

--- emacs.old/lisp/git.el
+++
@@ -1,7 +0,0 @@
-(use-package magit
-  ;:commands (magit-status magit-get-current-branch)
-  :config
-  (aqez/leader-key-def
-    "g" '(:ignore t :which-key "Magit")
-    "gg" '(magit-status :which-key "Open Magit"))
-  (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1))
