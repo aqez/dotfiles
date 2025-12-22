1196c1a3d5f1327c379f391f9c13f25a479b37d3
tony-dwire-paradigm <tony.dwire@paradigmh2o.com>
2025-12-22 12:22:19 -0500

    Remove obsolete leap call

--- nvim/lua/plugins/editing.lua
+++ nvim/lua/plugins/editing.lua
@@ -23,7 +23,9 @@ return {
             "tpope/vim-repeat"
         },
         config = function()
-            require('leap').create_default_mappings()
+            vim.keymap.set({'n', 'x', 'o'}, 's',  '<Plug>(leap-forward)')
+            vim.keymap.set({'n', 'x', 'o'}, 'S',  '<Plug>(leap-backward)')
+            vim.keymap.set('n',             'gs', '<Plug>(leap-from-window)')
         end
     },
     {
--- nvim/lua/plugins/notes.lua
+++ nvim/lua/plugins/notes.lua
@@ -1,5 +1,13 @@
 return {
     {
+        "3rd/image.nvim",
+        build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+        opts = {
+            backend = "sixel",
+            processor = "magick_cli",
+        }
+    },
+    {
         "obsidian-nvim/obsidian.nvim",
         version = "*",
         lazy = false,
@@ -11,7 +19,6 @@ return {
             workspaces = {
                 { name = "notes", path = "~/repos/notes" }
             },
-            --notes_subdir = "notes",
             daily_notes = {
                 folder = "dailies"
             },
