5a6edbe3b9553ceb93e06381293b148555f929a5
tony-dwire-paradigm <tony.dwire@paradigmh2o.com>
2025-12-22 13:45:35 -0500

    More markdown setup

--- nvim/lua/plugins/notes.lua
+++ nvim/lua/plugins/notes.lua
@@ -16,6 +16,7 @@ return {
             "nvim-lua/plenary.nvim"
         },
         opts = {
+            legacy_commands = false,
             workspaces = {
                 { name = "notes", path = "~/repos/notes" }
             },
@@ -23,27 +24,21 @@ return {
                 folder = "dailies"
             },
             checkbox = {
-                order = { " ", "~", "x" }
+                order = { " ", "x" }
             },
-            ui = {
-                checkboxes = {
-                    [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
-                    ["~"] = { char = "󰡖", hl_group = "ObsidianTilde" },
-                    ["x"] = { char = "", hl_group = "ObsidianDone" },
-                }
-            }
+            ui = { enable = false }
         },
         keys = {
             { "<leader>n",   group = "Notes" },
-            { "<leader>ns",  "<cmd>ObsidianSearch<cr>",    desc = "Search Notes" },
-            { "<leader>nn",  "<cmd>ObsidianNew<cr>",       desc = "New Note" },
+            { "<leader>ns",  "<cmd>Obsidian search<cr>",    desc = "Search Notes" },
+            { "<leader>nn",  "<cmd>Obsidian new<cr>",       desc = "New Note" },
 
             { "<leader>nd",  group = "Dailies",            desc = "Dailies" },
             --{ "<leader>ndd", ":ObsidianDailies<cr>",   desc = "Today" },
-            { "<leader>ndd", "<cmd>ObsidianDailies<cr>",   desc = "Today" },
-            { "<leader>ndt", "<cmd>ObsidianToday<cr>",     desc = "Today" },
-            { "<leader>ndy", "<cmd>ObsidianYesterday<cr>", desc = "Yesterday" },
-            { "<leader>ndT", "<cmd>ObsidianTomorrow<cr>",  desc = "Tomorrow" },
+            { "<leader>ndd", "<cmd>Obsidian dailies<cr>",   desc = "Dailies" },
+            { "<leader>ndt", "<cmd>Obsidian today<cr>",     desc = "Today" },
+            { "<leader>ndy", "<cmd>Obsidian yesterday<cr>", desc = "Yesterday" },
+            { "<leader>ndT", "<cmd>Obsidian tomorrow<cr>",  desc = "Tomorrow" },
         }
     }
 }
