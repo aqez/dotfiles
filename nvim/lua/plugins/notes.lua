bf4b5b243a26779e37de8f2532c019aff2bb99f8
tony-dwire-paradigm <tony.dwire@paradigmh2o.com>
2025-12-22 13:47:49 -0500

    Remove image stuff

--- nvim/lua/plugins/notes.lua
+++ nvim/lua/plugins/notes.lua
@@ -1,13 +1,5 @@
 return {
     {
-        "3rd/image.nvim",
-        build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
-        opts = {
-            backend = "sixel",
-            processor = "magick_cli",
-        }
-    },
-    {
         "obsidian-nvim/obsidian.nvim",
         version = "*",
         lazy = false,
