414e2bb23a6ed47ca4451d98f2874ed6df93582c
tony-dwire-paradigm <tony.dwire@paradigmh2o.com>
2026-03-02 12:16:22 -0500

    Add avante.nvim and fix lualine to use global status line

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -26,43 +26,6 @@ return {
         lazy = false,
     },
     {
-        'NTBBloodbath/doom-one.nvim',
-        lazy = false,
-        enable = false,
-        config = function()
-            -- Add color to cursor
-            vim.g.doom_one_cursor_coloring = false
-            -- Set :terminal colors
-            vim.g.doom_one_terminal_colors = true
-            -- Enable italic comments
-            vim.g.doom_one_italic_comments = false
-            -- Enable TS support
-            vim.g.doom_one_enable_treesitter = true
-            -- Color whole diagnostic text or only underline
-            vim.g.doom_one_diagnostics_text_color = false
-            -- Enable transparent background
-            vim.g.doom_one_transparent_background = true
-
-            -- Pumblend transparency
-            vim.g.doom_one_pumblend_enable = false
-            vim.g.doom_one_pumblend_transparency = 20
-
-            -- Plugins integration
-            vim.g.doom_one_plugin_neorg = true
-            vim.g.doom_one_plugin_barbar = false
-            vim.g.doom_one_plugin_telescope = true
-            vim.g.doom_one_plugin_neogit = true
-            vim.g.doom_one_plugin_nvim_tree = true
-            vim.g.doom_one_plugin_dashboard = true
-            vim.g.doom_one_plugin_startify = true
-            vim.g.doom_one_plugin_whichkey = true
-            vim.g.doom_one_plugin_indent_blankline = true
-            vim.g.doom_one_plugin_vim_illuminate = true
-            vim.g.doom_one_plugin_lspsaga = false
-            --vim.cmd [[colorscheme doom-one]]
-        end
-    },
-    {
         "folke/which-key.nvim",
         lazy = false,
         opts = {}
@@ -70,7 +33,11 @@ return {
     {
         "nvim-lualine/lualine.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons", "Mofiqul/vscode.nvim" },
-        opts = {}
+        config = function()
+            vim.o.laststatus = 3
+            require('lualine').setup({ globalstatus = true })
+            vim.o.laststatus = 3
+        end
     },
     {
         "stevearc/oil.nvim",
