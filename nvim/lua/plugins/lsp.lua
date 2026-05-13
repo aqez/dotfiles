2f1d11cde0c76c7caf02bf3b944514d7efd12975
tony-dwire-paradigm <tony.dwire@paradigmh2o.com>
2026-05-13 10:28:18 -0400

    Enable vim completions in my plugin files

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -48,4 +48,15 @@ return {
             -- your configuration comes here; leave empty for default settings
         }
     },
+    {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+            library = {
+                -- See the configuration section for more details
+                -- Load luvit types when the `vim.uv` word is found
+                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+            },
+        },
+    },
 }
