bc0b1d23f71dc53e2c72b92fb629d00201ec409f
tony-dwire-paradigm <tony.dwire@paradigmh2o.com>
2026-07-09 09:20:06 -0400

    Add file path copy

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -24,6 +24,20 @@ wk.add({
     { "<leader>l",  "<C-w>l",                            desc = "Move to right window" },
 
     { "<leader>p",  group = "Project" },
+    { "<leader>pf", function()
+        local path = vim.api.nvim_buf_get_name(0)
+        if path == "" then
+            vim.notify("No file loaded", vim.log.warn)
+            return
+        end
+        local root = vim.fs.root(0, ".git") or vim.fn.getcwd()
+        local rel = vim.fn.fnamemodify(path, ':')
+        if rel:sub(1, #root + 1) == root .. "/" then
+            rel = rel:sub(#root + 2)
+        end
+        vim.fn.setreg("+", rel)
+        vim.notify("Copied: " .. rel, vim.log.info)
+      end, desc = "Copy file path (relative to project root)" },
     { "<leader>pT", terminals.open_terminals_in_project, desc = "Open terminals in project" },
 
     { "[e",         ":cprev<CR>",                        desc = "Previous Quickfix" },
